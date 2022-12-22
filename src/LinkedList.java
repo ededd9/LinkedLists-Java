7c3d9543e92d97197133a755620a6e17e176382d
Edward Hurtado <edhur9@gmail.com>
2022-12-22 00:42:02 -0500

    Added removeNode/ getNodeAtIndex methods

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,11 +1,12 @@
 public class LinkedList {
     Node head;
     int size;
-
+    //constructor
     public LinkedList(){
         this.head = null;
         this.size = 0;
     }
+    //getCurrentNode method
     public Node getCurrentNode(){
         Node current = head;
         while(current.next != null){
@@ -13,14 +14,44 @@ public class LinkedList {
         }
         return current;
     }
+    //countNodes method
     public int countNodes(Node head){
         int count = 1;
         Node current = head;
+    
         while(current.next != null){
             count++;
             current = current.next;
         }
         return count;
+    }
+    //addNode method
+    public void addNode(Node node){
+        getCurrentNode().next = node;
+    }
+    public Node getNodeAtIndex(int index){
+        Node current = head;
+        
+        int count = 0;
+        while(current.next != null){
+            if(count == index){
+                System.out.println("Current nodes data: " + current.data);
+                return current;
+            }
+            count++;
+            current = current.next;
+        }
+        return null;
+    }
+    public void removeNode(Node node){
+        Node current = head;
+        while(current.next != null){
+            if(current == node){
+                current = null;
+                break;
+            }
+            current = current.next;
+        }
     }
    
 }
